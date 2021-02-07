12d3e8412c3964efdc7df94c36d89fadf57dd5df
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-02-07 21:34:52 +0500

    Добавьте файлы проекта.

---
+++ HW_1_06-02-2021/ArrayHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_1_06_02_2021
+{
+    class ArrayHelper<T>
+    {
+        public static T Pop(ref T[] array)
+        {
+            int arrLen = array.Length;
+            T delElem = array[arrLen - 1];
+            T[] newArr = new T[arrLen - 1];
+
+
+            for (int i = 0; i < arrLen - 1; i++)
+            {
+                newArr[i] = array[i];
+            }
+            array = newArr;
+            return delElem;
+        }
+        public static int Push(ref T[] array, T newElement)
+        {
+            int arrLen = array.Length;
+            T[] newArr = new T[arrLen + 1];
+            for (int i = 0; i < arrLen + 1; i++)
+            {
+                newArr[i] = (i == arrLen) ? newElement : array[i];
+            }
+            array = newArr;
+            return array.Length;
+        }
+        public static T Shift(ref T[] array)
+        {
+            int arrLen = array.Length;
+            T delElement = array[0];
+            T[] newArr = new T[arrLen - 1];
+            for (int i = 1; i < arrLen; i++)
+            {
+                newArr[i - 1] = array[i];
+            }
+            array = newArr;
+            return delElement;
+        }
+        public static int UnShift(ref T[] array, T newElement)
+        {
+            int arrLen = array.Length;
+            T[] newArr = new T[arrLen + 1];
+            for (int i = 0; i < arrLen + 1; i++)
+            {
+                newArr[i] = (i == 0) ? newElement : array[i - 1];
+            }
+            array = newArr;
+            return array.Length;
+        }
+        public static T[] Slice(ref T[] array, int? Begin_Index = null, int? End_Index = null)
+        {
+            int arrLen = array.Length;
+            Begin_Index = Begin_Index ?? 0;
+            End_Index = End_Index ?? arrLen;
+            if (End_Index < 0)
+            {
+                End_Index = arrLen + End_Index;
+            }
+            if (Begin_Index < 0)
+            {
+                Begin_Index = End_Index + Begin_Index;
+            }
+            int new_len = (End_Index - Begin_Index > 0) ? (int)(End_Index - Begin_Index) : 0;
+            Console.WriteLine($"New arrLen = {new_len}");
+            T[] newArr = new T[new_len];
+            for (int i = 0; i < new_len; i++)
+            {
+                newArr[i] = array[i + (int)Begin_Index];
+            }
+            return newArr;
+        }
+    }
+}
---
+++ HW_1_06-02-2021/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace HW_1_06_02_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[] { "C#", "JS", "C++", "Java", "Ruby" };
+            Console.WriteLine("Наш массив: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.Write("\n");
+            Console.WriteLine(@"Выберите команду:
+                    1. Pop -----> 1   
+                    2. Push ----> 2
+                    3. Shift ---> 3   
+                    4. Unshift--> 4
+                    5. Slice----> 5  
+                    6. Exit-----> 6");
+            int myCommand = int.Parse(Console.ReadLine());
+            while (myCommand != 6)
+            {
+                switch (myCommand)
+                {
+                    case 1:
+                        Console.WriteLine($"Удаленный элемент: {ArrayHelper<string>.Pop(ref array)}");
+                        Console.WriteLine("Наш изменённый массив:");
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            Console.Write($"{array[i]} ");
+                        }
+                        break;
+                    case 2:
+                        Console.Write("Добавляемый элемент = ");
+                        string addElement = Console.ReadLine();
+                        ArrayHelper<string>.Push(ref array, addElement);
+                        Console.WriteLine("Наш изменённый массив:");
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            Console.Write($"{array[i]} ");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine($"Удаленный элемент: {ArrayHelper<string>.Shift(ref array)}");
+                        Console.WriteLine("Наш изменённый массив:");
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            Console.Write($"{array[i]} ");
+                        }
+                        break;
+                    case 4:
+                        Console.Write("добавляемый элемент = ");
+                        addElement = Console.ReadLine();
+                        ArrayHelper<string>.UnShift(ref array, addElement);
+                        Console.WriteLine("Наш изменённый массив:");
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            Console.Write($"{array[i]} ");
+                        }
+                        break;
+                    case 5:
+                        int? begin = null, end = null;
+                        Console.WriteLine(@"Begin index:
+                            1. Нужно указывать ---> 0
+                            2. Не указывать    ---> 1");
+                        int enterNumber = int.Parse(Console.ReadLine());
+                        if (enterNumber == 0)
+                        {
+                            Console.Write("BeginIndex = ");
+                            begin = int.Parse(Console.ReadLine());
+                        }
+                        Console.WriteLine(@"End index:
+                            1. Нужно указывать ---> 0
+                            2. Не указывать    ---> 1");
+                        enterNumber = int.Parse(Console.ReadLine());
+                        if (enterNumber == 0)
+                        {
+                            Console.Write("EndIndex = ");
+                            end = int.Parse(Console.ReadLine());
+                        }
+                        string[] new_array = ArrayHelper<string>.Slice(ref array, begin, end);
+                        Console.WriteLine("Mассив: ");
+                        for (int i = 0; i < new_array.Length; i++)
+                        {
+                            Console.Write($"{new_array[i]} ");
+                        }
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        break;
+                }
+                if (myCommand != 6)
+                {
+                    Console.Write("\n");
+                    Console.WriteLine(@"Выберите команду:
+                    1. Pop -----> 1   
+                    2. Push ----> 2
+                    3. Shift ---> 3   
+                    4. Unshift--> 4
+                    5. Slice----> 5  
+                    6. Exit-----> 6");
+                    myCommand = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
